0b39e99f9ecbdb656382473733c01ba3b2bf6d02
Gary Armstrong <gary.armstrong@weather.com>
2022-01-21 15:50:09 -0500

    working example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::{arg, App};
+
 fn main() {
-    println!("Hello, world!");
+  let matches = App::new("patcat")
+              .version("0.1")
+              .author("Gary Armstrong")
+              .about("Like cat but for pattern visibility")
+              .arg(arg!(--file <FILENAME>))
+              .get_matches();
+
+  println!("File name {:?}", matches.value_of("file").expect("required"));
 }
