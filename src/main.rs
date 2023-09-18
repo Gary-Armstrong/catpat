a90fd68f499387a0e3350cc98e5b9743238d216e
Gary Armstrong <gary.armstrong@weather.com>
2023-09-18 16:16:24 -0400

    clap v4 and begin relearning

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use clap::{arg, App};
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  #[arg(short, long)]
+  name: String,
+
+  #[arg(short, long, default_value_t = 1)]
+  count: u8,
+}
 
 fn main() {
-  let matches = App::new("patcat")
-              .version("0.1")
-              .author("Gary Armstrong")
-              .about("Like cat but for pattern visibility")
-              .arg(arg!(--file <FILENAME>))
-              .get_matches();
 
-  println!("File name {:?}", matches.value_of("file").expect("required"));
+  let args = Args::parse();
+
+  // let matches = App::new("patcat")
+  //             .version("0.1")
+  //             .author("Gary Armstrong")
+  //             .about("Like cat but for pattern visibility")
+  //             .arg(arg!(--file <FILENAME>))
+  //             .get_matches();
+
+  println!("name {:?}", args.name)
 }
